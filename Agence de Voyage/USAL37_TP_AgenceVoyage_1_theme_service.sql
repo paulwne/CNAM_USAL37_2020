e11e23a190b7e2d65c7cacd51b15eecee79221a0
paulwne <73827890+paulwne@users.noreply.github.com>
2020-12-09 10:59:33 +0100

    modification

--- Agence de Voyage/USAL37_TP_AgenceVoyage_1_theme_service.sql
+++ Agence de Voyage/USAL37_TP_AgenceVoyage_1_theme_service.sql
@@ -7,14 +7,14 @@ USE usal37_agence;
 CREATE TABLE theme
 (
 theme_code CHAR(32) PRIMARY KEY,
-theme_name VARCHAR(32) NOT NULL,
+theme_name VARCHAR(32) NOT NULL UNIQUE,
 theme_description VARCHAR (255)
-)
+);
 
-CREATE TABLE service;
+CREATE TABLE service
 (
 service_code CHAR(32) PRIMARY KEY,
-service_name VARCHAR(32) NOT NULL,
+service_name VARCHAR(32) NOT NULL UNIQUE,
 service_description CHAR(255) ,
 service_score CHAR(32)NOT NULL,
 service_commentaire VARCHAR(255)
