938a89c5e499bd64c9948594e649757040399093
paulwne <73827890+paulwne@users.noreply.github.com>
2020-12-09 12:07:55 +0100

    update

--- Agence de Voyage/USAL37_TP_AgenceVoyage_1_theme_service.sql
+++ Agence de Voyage/USAL37_TP_AgenceVoyage_1_theme_service.sql
@@ -4,18 +4,18 @@ CREATE DATABASE usal37_agence DEFAULT CHARACTER SET 'utf8';
 
 USE usal37_agence;
 
-CREATE TABLE theme
+CREATE TABLE themes
 (
-theme_code CHAR(32) PRIMARY KEY,
-theme_name VARCHAR(32) NOT NULL UNIQUE,
-theme_description VARCHAR (255)
+theme_code INT PRIMARY KEY AUTO_INCREMENT,
+theme_name VARCHAR(32) NOT NULL,
+theme_description MEDIUMTEXT
 );
 
-CREATE TABLE service
+CREATE TABLE services
 (
-service_code CHAR(32) PRIMARY KEY,
-service_name VARCHAR(32) NOT NULL UNIQUE,
-service_description CHAR(255) ,
+service_code INT PRIMARY KEY AUTO_INCREMENT,
+service_name VARCHAR(32) NOT NULL,
+service_description MEDIUMTEXT NULL,
 service_score CHAR(32)NOT NULL,
 service_commentaire VARCHAR(255)
 );
